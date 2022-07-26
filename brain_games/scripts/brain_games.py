12b90c8804143739b212b252c825b995ff281042
alexanderthecreator <alexander.rastegaev@gmail.com>
2022-07-26 15:08:04 +0500

    modify brai_games.py, cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+from brain_games.cli import welcome_user
 
 def main():
     print('Welcome to the Brain Games!')
 
-
-
 if __name__ == '__main__':
     main()
+
+
+brain_games.cli.welcome_user()
