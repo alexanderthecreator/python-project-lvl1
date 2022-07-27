125e198b89ee5668e8c5a1a4baa1b41adee6535a
alexanderthecreator <alexander.rastegaev@gmail.com>
2022-07-27 17:01:21 +0500

    modify game_even.py

--- brain_games/game_modules/game_even.py
+++ brain_games/game_modules/game_even.py
@@ -2,34 +2,42 @@
 
 import prompt
 from random import randint
-from brain_games.greeting import name
 
 
 def game():
+      #Объявление переменной принимающей значение введенного имени игрока
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
     print('Answer "yes" if the number is even, otherwise "no"')
-    i = 1
+      #Задаем счетчик циклов игры
+    correct_answers = 0
     result = ""
-    while i <= 3:
+    while correct_answers < 3:
+        #Объявление переменной принимающей значение счетчика случайных чисел
         question = randint(1, 100)
         print(f'Question: {question}')
-        answer = prompt.string('')
-        print(f'Your answer: {answer}')
-        if question % 2 == 0:             
+        #Объявление переменной принимающей значение введенного ответа игрока
+        answer = prompt.string('Your answer: ')
+        #Проверка четности значения переменной принявшей значение случайного числа
+        if question % 2 == 0:
+            #Проверка соответствия ответа пользователя значению переменной принявшей случайное число
             if answer.lower() == 'yes':
                 print('Correct!')
-                result = "Correct"
-            else: 
-                print(f'{answer} is wrong answer ;(. Correct answer was "yes"')
+                correct_answers = correct_answers + 1
+            else:
+                print(f"{answer} is wrong answer ;(.\n Correct answer was 'yes'.\n Let's try again, {name}")
+                break
+        #Проверка нечетности значения переменной принявшей значение случайного числа
         elif question % 2 != 0:
             if answer.lower() == 'no':
                 print('Correct!')
-                result = "Correct"
-            else: 
-                print(f'{answer} is wrong answer ;(. Correct answer was "no"')
-        if result == "Correct":
-            print(f'Congratulations, {name}')
-            i = i + 1
-        else:
-            break
+                correct_answers = correct_answers + 1                
+            else:
+                print(f"{answer} is wrong answer ;(.\n Correct answer was 'no'.\n Let's try again, {name}")
+                break
+    #Проверка условия количества правленьных ответов
+    if correct_answers == 3:
+        print(f'Congratulations, {name}!')
+
+
 game()
-    
