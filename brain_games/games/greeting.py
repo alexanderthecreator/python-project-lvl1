6aec6a40bb1e55a0815d1568454dd0ef225b0968
alexanderthecreator <alexander.rastegaev@gmail.com>
2022-07-28 23:47:26 +0500

    add module greeting.py as common logic

---
+++ brain_games/games/greeting.py
@@ -0,0 +1,10 @@
+"""Приветствие игры"""
+
+import prompt
+
+
+def greeting():
+    print('Welcome to the Brain Games!')
+    user_name= prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    return user_name
