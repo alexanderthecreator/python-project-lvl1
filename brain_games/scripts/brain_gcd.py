b8cdf76177d968493e7b124af882b8a4f4b5f35b
alexanderthecreator <alexander.rastegaev@gmail.com>
2022-07-31 19:08:38 +0500

    modify brain_games/scripts/brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-import brain_games.games.game_gcd
+import brain_games.games.game_gcd import game
 
 
 def main():
-    brain_games.games.game_gcd.game()
+    game()
 
 if __name__ == '__main__':
     main()
