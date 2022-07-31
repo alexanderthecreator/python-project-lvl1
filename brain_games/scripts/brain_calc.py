7f44ef3ce45a42a1c1f0ebad66418418606bcea0
alexanderthecreator <alexander.rastegaev@gmail.com>
2022-07-31 15:57:32 +0500

    modify ../scripts/brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+import brain_games.games.game_calc
+from brain_games.games.greeting import greeting
+
+def main():
+    brain_games.games.game_calc.game()  
+
+if __name__ == '__main__':
+    main()
