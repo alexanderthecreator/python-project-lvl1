9557029ccc5d13e61263a4d6ab46d11139b197f9
alexanderthecreator <alexander.rastegaev@gmail.com>
2022-08-02 18:35:57 +0500

    modify brain_games/scripts/brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-from brain_games.games.game_prime import game
+from brain_games.games.game_prime import game, GAME_INSTRUCTIONS
+from brain_games.games.engine import user_greeting, game_engine 
+
 
 
 def main():
-    game()
+    game_engine(game, GAME_INSTRUCTIONS)
 
 if __name__ == '__main__':
     main()
