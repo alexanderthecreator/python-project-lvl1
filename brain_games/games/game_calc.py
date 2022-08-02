ba9f1ed0c1f85e4999634500fbaac8b952c24953
alexanderthecreator <alexander.rastegaev@gmail.com>
2022-08-02 17:53:06 +0500

    modify brain_games/games/game_calc.py

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -1,56 +1,29 @@
 """Игра - калькулятор"""
 
-import prompt
+
 from random import randint, choice
 
-print('Welcome to the Brain Games!')
-user_name= prompt.string('May I have your name? ')
-print(f'Hello, {user_name}!')
+
+GAME_INSTRUCTIONS = 'What is the result of the expression?'
+
 
 def game():
-    print('What is the result of the expression?')
-    correct_answers = 0
-    while correct_answers < 3:
-        first_member = randint(1, 20)
-        operator = choice(["+", "-", "*"])
-        second_member = randint(1, 20)        
-        if operator == "+":
-            print(f'Question: {first_member} {operator} {second_member}')
-            user_answer = prompt.string('Your answer: ')
-            correct_answer = str(first_member + second_member)
-            if correct_answer == user_answer:
-                print('Correct!')
-                correct_answers = correct_answers + 1
+    first_member = randint(1, 20)
+    operator = choice(["+", "-", "*"])
+    second_member = randint(1, 20)        
+    if operator == "+":
+        print(f'Question: {first_member} {operator} {second_member}')
+        correct_answer = str(first_member + second_member)
+    elif operator == "-":               
+        while first_member < second_member:
+            first_member = randint(1, 20)
+            second_member = randint(1,20)    
+            if first_member < second_member:
+                continue
             else:
-                print(f"{user_answer} is wrong answer ;(\nCorrect answer was {correct_answer}\nLet's try again, {user_name}!")
-                break
-        elif operator == "-":               
-            while first_member < second_member:
-                first_member = randint(1, 20)
-                second_member = randint(1,20)    
-                if first_member < second_member:
-                    continue
-                else:
-                    print(f'Question: {first_member} {operator} {second_member}')
-                    user_answer = prompt.string('Your answer: ')
-                    correct_answer = str(first_member - second_member)
-                    if correct_answer == user_answer:
-                        print('Correct!')
-                        correct_answers = correct_answers + 1
-                    else:
-                        print(f"{user_answer} is wrong answer ;(\nCorrect answer was {correct_answer}\nLet's try again, {user_name}!")
-                        break
-        elif operator == "*":
-            print(f'Question: {first_member} {operator} {second_member}')
-            user_answer = prompt.string('Your answer: ')
-            correct_answer = str(first_member * second_member)
-            if correct_answer == user_answer:
-                print('Correct!')
-                correct_answers = correct_answers + 1
-            else:
-                print(f"{user_answer} is wrong answer ;(\nCorrect answer was {correct_answer}\nLet's try again, {user_name}!")
-                break
-
-    #Проверка условия количества правильных ответов
-    if correct_answers == 3:
-        print(f'Congratulations, {user_name}!')
+                print(f'Question: {first_member} {operator} {second_member}')
+                correct_answer = str(first_member - second_member)
+    elif operator == "*":
+        print(f'Question: {first_member} {operator} {second_member}')
+        correct_answer = str(first_member * second_member)
+    return correct_answer 
