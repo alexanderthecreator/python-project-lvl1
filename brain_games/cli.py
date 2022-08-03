b571ddd2d10f597c7bef174deea5f14a65250a37
alexanderthecreator <alexander.rastegaev@gmail.com>
2022-08-03 12:13:25 +0500

    modify brain_games/cli.py according to linter

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,9 @@
+"""User greeting."""
+
+
 import prompt
+
 
 def welcome_user():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-# Почему, если в конце файла добавить вызов объявленной функции, то этот вызов дулируется при исполнении модуля ./scripts/brain_games.py, ведь там мы импортировали только саму функцию, а не модуль целиком?
