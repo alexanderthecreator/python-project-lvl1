7f5227f501d5d884a227aa366a9b7437bb04bb7d
alexanderthecreator <alexander.rastegaev@gmail.com>
2022-08-03 12:11:07 +0500

    modify brain_games/games/game_calc.py according to linter

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -1,4 +1,4 @@
-"""Игра - калькулятор"""
+"""Игра - калькулятор."""
 
 
 from random import randint, choice
@@ -8,15 +8,16 @@ GAME_INSTRUCTIONS = 'What is the result of the expression?'
 
 
 def game():
+
     first_member = randint(1, 20)
     operator = choice(["+", "-", "*"])
-    second_member = randint(1, 20)        
+    second_member = randint(1, 20)
     if operator == "+":
         print('Question: ', end='')
         print(first_member, operator, second_member, sep=' ')
         correct_answer = str(first_member + second_member)
         return correct_answer
-    elif operator == "-":               
+    elif operator == "-":
         while first_member <= second_member:
             first_member = randint(1, 20)
             second_member = randint(1,20)    
