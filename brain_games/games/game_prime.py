e879bdf6fa741a4b5bf8369b203eb20d1b606b0e
alexanderthecreator <alexander.rastegaev@gmail.com>
2022-08-03 13:14:46 +0500

    linter changes

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -3,9 +3,10 @@
 from random import randint
 
 
-GAME_INSTRUCTIONS = (f'Answer "yes" if given number is prime.'
-                    f' Otherwise answer "no".')
-
+GAME_INSTRUCTIONS = (
+    f'Answer "yes" if given number is prime.{" "}'
+    f'Otherwise answer "no".'
+)
 
 def game():
     question = randint(1, 1000)
@@ -17,6 +18,3 @@ def game():
         else:
             correct_answer = 'yes'
     return correct_answer
-
-
-print(GAME_INSTRUCTIONS)
